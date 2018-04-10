97f0a562edb01fbb66f5dcd1534d74b5cef6b257
kbataran <kbataran@localytics.com>
2018-04-10 15:17:57 -0400

    update with buttons to sign in/out etc.

--- LocalyticsGDPR/App.js
+++ LocalyticsGDPR/App.js
@@ -1,36 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import LLLocalytics from 'localytics-react-native';
 
 export default class App extends React.Component {
 
-  componentWillMount() {
-
-    var optOutStatus = false;
-    //make server call to get opt out status and, if applicable, authenticate the user
-
-    //if setting customer Id, use the following:
-    LLLocalytics.setCustomerIdWithPrivacyOptedOut("id123", optOutStatus);
-    //if not setting customer Id, use the following:
-    //Localytics.setPrivacyOptedOut(optOutStatus);
-
-    //you've set the opted out flag to the proper value, you can now safely turn on uploads
-    LLLocalytics.pauseDataUploading(false);
-
-    //ensure you're not sending Places notifications to users who have opted out of tracking (requires Places implementation)
-    //LLLocalytics.setLocationMonitoringEnabled(!optOutStatus);
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button onPress={signInTapped} title="Sign in"/>
+        <Button onPress={optInTapped} title="Opt-in data collection"/>
+        <Button onPress={optOutTapped} title="Opt-out data collection"/>
+        <Button onPress={signOutTapped} title="Sign out"/>
       </View>
     );
   }
 }
+
+const optInTapped = () => {
+  //TODO update your server with the latest opt-out status of this user
+  LLLocalytics.setPrivacyOptedOut(false);
+};
+
+const optOutTapped = () => {
+  //TODO update your server with the latest opt-out status of this user
+  LLLocalytics.setPrivacyOptedOut(true);
+
+  //Ensure you're not sending Places notifications to users who have opted out of tracking (requires Places implementation)
+  //LLLocalytics.setLocationMonitoringEnabled(!optOutStatus);
+};
+
+const signInTapped = () => {
+  //TODO Make a server call to authenticate the user and fetch their customerId
+  //and opt-out status
+  var optedOut = false;
+  var customerId = "id123";
+  LLLocalytics.setCustomerIdWithPrivacyOptedOut(customerId, optedOut);
+
+  //You've set the opted out flag to the proper value, you can now safely turn on uploads
+  LLLocalytics.pauseDataUploading(false);
+
+  //Ensure you're not sending Places notifications to users who have opted out
+  //of tracking (this requires Places implementation)
+  //LLLocalytics.setLocationMonitoringEnabled(!optedOut);
+};
+
+const signOutTapped = () => {
+  LLLocalytics.setCustomerIdWithPrivacyOptedOut("", false);
+};
 
 const styles = StyleSheet.create({
   container: {
