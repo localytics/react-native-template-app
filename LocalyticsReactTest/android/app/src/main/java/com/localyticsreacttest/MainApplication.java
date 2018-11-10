f7d7dd6622d429860ac0daf47b44547793021569
Anand Bashyam <abashyam@localytics.com>
2018-11-09 19:59:22 -0500

    First pass at update of react.

--- LocalyticsReactTest/android/app/src/main/java/com/localyticsreacttest/MainApplication.java
+++ LocalyticsReactTest/android/app/src/main/java/com/localyticsreacttest/MainApplication.java
@@ -29,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
           new LLLocalyticsPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
